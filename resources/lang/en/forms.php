e2ed6883acb9c822a283477c50b31d50ff0bee27
Alain Martini <webmaster@inalto.com>
2020-10-04 17:11:19 +0200

    correzione login

---
+++ resources/lang/en/forms.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "register" => [
+        "confirm_password" => "Confirm password",
+        "email_address" => "Email address",
+        "name" => "Name",
+        "password" => "Password",
+        "register" => "Register"
+    ]
+];
