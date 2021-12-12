9460721159614c51a6c0da5840cee1b78a4bf895
Alain Martini <webmaster@inalto.com>
2021-12-12 17:25:28 +0100

    modifiche funzioni di import

---
+++ app/Console/Commands/UserUpdate.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Support\Inalto\Import\User as UserImport;
+
+class UserUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'inalto:user_update {--uid=0 : userid to import } { --truncate : clear previously imported avatars } {--dry-run: run without import }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Syncronize old inalto users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->output->writeln('Updating inalto users...');
+        return UserImport::import($this->output,$this->option('uid'),$this->option('truncate'),$this->option('dry-run'));
+        $this->output->writeln('Done.');
+
+    }
+}
