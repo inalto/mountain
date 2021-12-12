b181f145840bfbde57425e92bbbe5b5d53dac1e5
Alain Martini <webmaster@inalto.com>
2021-12-12 17:26:56 +0100

    creato parser bollini sentieri e strade

---
+++ app/Support/Inalto/ParseReport.php
@@ -0,0 +1,118 @@
+<?php
+namespace App\Support\Inalto;
+
+use  App\Support\Inalto\Signs;
+
+class ParseReport
+{
+
+
+    public static function beautify($payload)
+    {
+        $payload=self::timeParser($payload);
+        $payload=self::routeSquareParser($payload);
+        $payload=self::routeRoundParser($payload);
+        $payload=self::routeTriangleParser($payload);
+        return $payload;
+    }
+
+    public static function timeParser($payload)
+    {
+        preg_match_all('/\[\d{1,2}h\d{1,2}\'[A-Z]{1,2}\d\]/',$payload,$matches);
+        $occurrences=array_shift($matches);
+        $replace=[];
+        foreach ($occurrences as $occurrence){
+            preg_match('/\'[A-Z]{1,2}\d/',$occurrence,$class);
+        
+            $class=strtolower(ltrim(array_shift($class),'\''));
+            preg_match('/\d{1,2}h\d{1,2}\'/',$occurrence,$content);
+            $time=array_shift($content);
+
+            $replace[] = '<span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white rounded '.$class.'">'.$time.'</span>';
+        }
+        return str_replace($occurrences,$replace,$payload);
+
+    }
+
+    public static function routeSquareParser($payload)
+    {
+        preg_match_all('/\[([a-zA-Z0-9 ]+)\]([a-zA-Z])/m',$payload,$matches);
+        if (count($matches)<2 || empty($matches[0]) || empty($matches[1]) || empty($matches[2])) return $payload;
+        $replace=[];
+        $occurrences=$matches[0];
+        $contents=$matches[1];
+        $types=$matches[2];
+        for ($i=0;$i<count($occurrences);$i++)
+        {
+            switch ($types[$i]){
+              case 'c':
+                $replace[] = Signs::cai($contents[$i]);
+              break;
+              case 'g':
+                $replace[] = Signs::rectYellow($contents[$i]);
+                break;
+            case 'b':
+                $replace[] = Signs::rectBlue($contents[$i]);
+                break;
+            case 'v':
+                $replace[] = Signs::rectGreen($contents[$i]);
+                break;
+                
+              default:
+               $replace[]="<small>unable to parse</small>";
+               break;
+            }
+        }
+        return str_replace($occurrences,$replace,$payload);
+    }
+    
+
+    public static function routeRoundParser($payload)
+    {
+        preg_match_all('/\(([a-zA-Z0-9 ]+)\)([a-z])/m',$payload,$matches);
+        if (count($matches)<2 || empty($matches[0]) || empty($matches[1]) || empty($matches[2])) return $payload;
+        $replace=[];
+        
+        $occurrences=$matches[0];
+        $contents=$matches[1];
+        $types=$matches[2];
+        for ($i=0;$i<count($occurrences);$i++)
+        {
+            
+            switch ($types[$i]){
+                case 'g':
+                    $replace[] = Signs::roundYellow($contents[$i]);
+                break;
+                default:
+                    $replace[]="<small>unable to parse</small>";
+                break;
+            }
+        }
+        return str_replace($occurrences,$replace,$payload);
+    }
+
+    public static function routeTriangleParser($payload)
+    {
+        preg_match_all('/\/([a-zA-Z0-9 ]+)\\\\([a-z])/m',$payload,$matches);
+        if (count($matches)<2 || empty($matches[0]) || empty($matches[1]) || empty($matches[2])) return $payload;
+        $replace=[];
+        $occurrences=$matches[0];
+        $contents=$matches[1];
+        $types=$matches[2];
+        
+        for ($i=0;$i<count($occurrences);$i++)
+        {
+            
+            switch ($types[$i]){
+                case 'g':
+                    $replace[] = Signs::triangleYellow($contents[$i]);
+                break;
+                default:
+                    $replace[]="<small>unable to parse</small>";
+                break;
+            }
+        }
+        return str_replace($occurrences,$replace,$payload);
+    }
+
+}
---
+++ app/Support/Inalto/Signs.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Support\Inalto;
+//use App\Models\Report;
+
+class Signs
+{
+
+
+    public static function cai($content)
+    {
+
+        return
+        '<span class="inline-flex w-12 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 60 30" version="1.1" style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+            <g transform="matrix(1,0,0,1,-104.187,-0.292386)">
+                <g id="cai" transform="matrix(1.14216,0,0,1.27195,103.959,-46.5204)">
+                    <rect x="0.199" y="36.804" width="52.532" height="23.586" style="fill:none;"/>
+                    <g transform="matrix(0.87864,0,0,0.513333,-91.4138,37.1274)">
+                        <rect x="105.264" y="0.901" width="57.795" height="10.721" style="fill:rgb(216,0,0);"/>
+                    </g>
+                    <g transform="matrix(0.87864,0,0,0.513333,-91.4138,53.6375)">
+                        <rect x="105.264" y="0.901" width="57.795" height="10.721" style="fill:rgb(216,0,0);"/>
+                    </g>
+                    <g transform="matrix(0.889135,0,0,0.803796,-0.315556,7.69392)">
+                        <rect x="1.564" y="37.194" width="57.113" height="27.387" style="fill:none;stroke:black;stroke-width:0.98px;"/>
+                    </g>
+                    <g transform="matrix(0.875535,0,0,0.786194,1.30479,36.7562)">
+                        <text x="50%" y="19.359px" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:12px;">'.$content.'</text>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        </span>';
+    }
+
+    public static function rectYellow($content)
+    {
+
+        return
+        '<span class="inline-flex w-12 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 60 30" version="1.1" style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+            <g transform="matrix(1,0,0,1,-40.2519,0)">
+                <g id="quadrato" transform="matrix(1.14216,0,0,1.27195,40.0241,-46.8128)">
+                    <rect x="0.199" y="36.804" width="52.532" height="23.586" style="fill:none;"/>
+                    <g transform="matrix(0.889135,0,0,0.803796,-0.315556,7.69392)">
+                        <rect x="1.564" y="37.194" width="57.113" height="27.387" style="fill:rgb(255,198,0);stroke:black;stroke-width:0.98px;"/>
+                    </g>
+                    <g transform="matrix(0.875535,0,0,0.786194,1.30479,36.7562)">
+                        <text x="50%" y="19.359px" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:12px;">'.$content.'</text>
+                    </g>
+                </g>
+            </g>
+        </svg></span>';
+    }
+
+    public static function rectGreen($content)
+    {
+
+        return
+        '<span class="inline-flex w-12 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 60 30" version="1.1" style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect id="auto" x="-0" y="0" width="60" height="30" style="fill:none;"/><path d="M59,4c-0,-1.657 -1.343,-3 -3,-3c-9.983,-0 -42.017,-0 -52,-0c-1.657,-0 -3,1.343 -3,3c-0,5.382 -0,16.618 -0,22c0,1.657 1.343,3 3,3c9.983,-0 42.017,-0 52,-0c1.657,0 3,-1.343 3,-3c-0,-5.382 -0,-16.618 -0,-22Z" style="fill:#268807;"/><path d="M56.611,6.007c-0,-1.657 -1.344,-3 -3,-3c-9.347,0 -37.875,0 -47.222,0c-1.656,0 -3,1.343 -3,3c0,4.608 0,13.378 0,17.986c0,1.657 1.344,3 3,3c9.347,-0 37.875,-0 47.222,-0c1.656,-0 3,-1.343 3,-3c-0,-4.608 -0,-13.378 -0,-17.986Z" style="fill:none;stroke:#ebebeb;stroke-width:1px;"/><text x="50%" y="19.107px" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:12px;fill:#ebebeb;">'.$content.'</text></svg></span>';
+    }
+
+    public static function rectBlue($content)
+    {
+
+        return
+        '<span class="inline-flex w-12 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 60 30" version="1.1" style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect id="ss" x="0" y="0" width="60" height="30" style="fill:none;"/><path d="M59,4c-0,-1.657 -1.343,-3 -3,-3c-9.983,-0 -42.017,-0 -52,-0c-1.657,-0 -3,1.343 -3,3c0,5.382 0,16.618 0,22c0,1.657 1.343,3 3,3c9.983,-0 42.017,-0 52,-0c1.657,0 3,-1.343 3,-3c0,-5.382 0,-16.618 -0,-22Z" style="fill:#1b4086;"/><path d="M56.611,6.007c-0,-1.657 -1.344,-3 -3,-3c-9.347,0 -37.875,0 -47.222,0c-1.656,0 -3,1.343 -3,3c0,4.608 0,13.378 0,17.986c0,1.657 1.344,3 3,3c9.347,-0 37.875,-0 47.222,-0c1.656,-0 3,-1.343 3,-3c-0,-4.608 -0,-13.378 -0,-17.986Z" style="fill:none;stroke:#ebebeb;stroke-width:1px;"/><text x="50%" y="19.107px" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:12px;fill:#ebebeb;">'.$content.'</text></svg></span>';
+    }
+
+    public static function roundYellow($content)
+    {
+
+        return
+        '<span class="inline-flex w-6 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 30 30" version="1.1" style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+            <g id="tondo" transform="matrix(0.322969,0,0,0.333387,-0.5625,-0.33871)">
+                <rect x="1.742" y="1.016" width="92.888" height="89.985" style="fill:none;"/>
+                <g transform="matrix(3.01026,0,0,2.91619,3.65087,2.7015)">
+                    <circle cx="14.794" cy="14.851" r="14.4" style="fill:rgb(255,198,0);stroke:black;stroke-width:1.03px;"/>
+                </g>
+                <g transform="matrix(3.09627,0,0,2.99952,5.65076,0.833791)">
+                    <text x="50%" y="19.359px" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:12px;">'.$content.'</text>
+                </g>
+            </g>
+        </svg></span>
+        ';
+    }
+
+    public static function triangleYellow($content)
+    {
+        return '<span class="inline-flex w-6 h-6 relative"><svg width="100%" height="100%" viewBox="0 0 30 30" version="1.1"  style="position:absolute;top:6px;fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><rect id="triangolo" x="0" y="0" width="30" height="30" style="fill:none;"/><clipPath id="_clip1"><rect x="0" y="0" width="30" height="30"/></clipPath><g clip-path="url(#_clip1)"><path d="M15,0l15,30l-30,0l15,-30Z" style="fill:#ffc600;"/><path d="M15,-0l15,30l-30,0l15,-30Zm-0,2.236l13.382,26.764c-0,0 -26.764,0 -26.764,0l13.382,-26.764Z"/><text x="50%" y="20px"  dominant-baseline="middle" text-anchor="middle" style="font-family:\'ArialMT\', \'Arial\', sans-serif;font-size:10px;">'.$content.'</text></g></svg></span>';
+    }
+}
